ccfd389a0eea37601fb6436568884636467334bb
Jan Murić <67594178+MuricJ@users.noreply.github.com>
2025-03-17 10:20:38 +0000

    Delete src directory

--- src/StaticEvaluation.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "Board.h"
-
-double MonteCarloStaticEval(Board board, int last_move, int playoffs);
--- src/types.h
+++
@@ -1,6 +0,0 @@
-#ifndef typesGuard
-#define typesGuard
-
-enum class BoardState {UNKNOWN=0, WON_0, WON_1, DRAWN, IN_PROGRESS}; // UNKNOWN must be default and therefore 0
-
-#endif
